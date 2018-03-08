9e846ff07795213b64f32232c44e1ffc07901a33
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-03-08 19:17:34 +1100

    1.1

--- popup.js
+++ popup.js
@@ -61,22 +61,89 @@ function saveChoice(url, choice) {
     chrome.storage.sync.set(items);
 }
 
+function getSavedTick() {
+    chrome.storage.sync.get("ticked", function (obj) {
+        console.log(obj['ticked']);
+        page.checked = obj['ticked'];
+        setMode(dropdown.value, url);
+    });
+}
+
+function saveTick() {
+    chrome.storage.sync.set({
+        'ticked': page.checked
+    });
+}
+
+function timerRun(time, url) {
+    if (!time) {
+        return
+    }
+
+    if (time == '15min') {
+        timerGo(0, 15, url);
+    } else if (time == '30min') {
+        timerGo(0, 3, url);
+    } else if (time == '45min') {
+        timerGo(0, 45, url);
+    } else if (time == '1hour') {
+        timerGo(1, 0, url);
+    }
+
+}
+
+function timerGo(hour, minute, url) {
+    startTime = new Date();
+    loop = window.setInterval(function() {
+        var now = new Date();
+        if (now.getSeconds()+now.getHours() === startTime.getSeconds()+startTime.getHours() + minute + hour) {
+            setMode('enabled', url);
+            dropdown.value = 'enabled';
+            saveChoice(url, 'enabled');
+            timer.value = '';
+            clearInterval(loop);
+        }
+    }, 600)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     getCurrentTabUrl((url) => {
         var dropdown = document.getElementById('dropdown');
+        var page = document.getElementById('page');
+        var timer = document.getElementById('timer');
         getSavedChoice(url, (savedChoice) => {
             if (savedChoice) {
                 setMode(savedChoice, url);
                 dropdown.value = savedChoice;
             }
+
+            getSavedTick();
+
             dropdown.addEventListener('change', () => {
                 setMode(dropdown.value, url);
                 saveChoice(url, dropdown.value);
             });
             page.addEventListener('change', () => {
                 setMode(dropdown.value, url);
-                saveChoice(url, dropdown.value);
+                saveTick();
             });
+
+            timer.addEventListener('change', () => {
+                timerRun(timer.value, url);
+            });
+        });
+
+        document.getElementById('button').addEventListener('click', function () {
+            var choice = dropdown.value;
+            if (choice == 'enabled') {
+                setMode('disabled', url);
+                saveChoice(url, 'disabled');
+                dropdown.value = 'disabled';
+            } else {
+                setMode('enabled', url);
+                saveChoice(url, 'enabled');
+                dropdown.value = 'enabled';
+            }
         });
     });
 });
