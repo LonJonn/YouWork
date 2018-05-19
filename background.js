f559faf8d898b412eb1559dca8b200cd940176c9
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-05-20 01:08:40 +1000

    actually this time

--- background.js
+++ background.js
@@ -1,36 +1,47 @@
-function backTimer(minute, ticked) {
-    startTime = new Date().getTime();
-    timer = setInterval(function () {
-        now = new Date().getTime();
-        if (now >= startTime + minute*60*1000) {
-            chrome.storage.sync.set({
-                'timerOption': ''
-            });
-            chrome.storage.sync.set({
-                'choice': 'enabled'
-            });
-            chrome.tabs.insertCSS(null, {
-                code: "#items.ytd-watch-next-secondary-results-renderer {visibility: hidden;} !important"
-            })
-            if (ticked) {
-                chrome.tabs.insertCSS(null, {
-                    code: "#dismissable.style-scope.ytd-shelf-renderer {visibility: hidden;} !important"
-                });
-                chrome.storage.sync.set({
-                    'ticked': true
-                });
-            } else if (!ticked) {
-                chrome.tabs.insertCSS(null, {
-                    code: "#dismissable.style-scope.ytd-shelf-renderer {visibility: visible;} !important"
-                })
-            }
-            chrome.runtime.sendMessage('done');
-            clearInterval(timer);
-        }
-    }, 60000)
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason == 'install') {
+    chrome.storage.sync.set({ on: false })
+    console.log('initiallise')
+  }
+})
 
-}
+chrome.tabs.onActivated.addListener(function() {
+  injectCSS()
+})
 
-function endTimer() {
-    clearInterval(timer);
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
+  if (info.status === 'complete') {
+    injectCSS()
+  }
+})
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.storage.sync.get('on', data => {
+    chrome.storage.sync.set({ on: !data.on })
+    injectCSS()
+  })
+})
+
+function injectCSS() {
+  chrome.storage.sync.get('on', data => {
+    if (data.on) {
+      chrome.tabs.insertCSS(null, {
+        code:
+          '#items.ytd-watch-next-secondary-results-renderer {visibility: hidden;} !important'
+      })
+      chrome.tabs.insertCSS(null, {
+        code:
+          '#dismissable.style-scope.ytd-shelf-renderer {visibility: hidden;} !important'
+      })
+    } else {
+      chrome.tabs.insertCSS(null, {
+        code:
+          '#items.ytd-watch-next-secondary-results-renderer {visibility: visible;} !important'
+      })
+      chrome.tabs.insertCSS(null, {
+        code:
+          '#dismissable.style-scope.ytd-shelf-renderer {visibility: visible;} !important'
+      })
+    }
+  })
 }
